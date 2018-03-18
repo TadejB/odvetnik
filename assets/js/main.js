a48c960962437d0772239c80f600cd01fce399af
TadejB <tadejbirtic@gmail.com>
2018-03-18 23:44:41 +0100

    working version, todo navbar selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,11 @@ var html = document.querySelector('html');
 var changeOverflow = document.querySelectorAll('.change-overflow');
 var sectionContact = document.querySelector('#section-contact');
 var fakeNavbar = document.querySelector('.fake-navbar');
+var contact = document.querySelector('#contact');
 var presentation = document.querySelector('#presentation');
+var profession = document.querySelector('#profession');
+var links = document.querySelector('#links');
+var gallery = document.querySelector('#gallery');
 var sectionPresentation = document.querySelector('#section-presentation');
 var sectionProfession = document.querySelector('#section-profession');
 var sectionLinks = document.querySelector('#section-links');
@@ -21,12 +25,10 @@ var allSections = document.querySelectorAll('.add-section-padding');
 var anchor = document.querySelectorAll('.anchor');
 var detailsProfession = document.querySelectorAll('.profession-details-main');
 var contentWrapper = document.querySelector('.content-wrapper');
+var sectionGallery = document.querySelector('#section-gallery');
 
 window.addEventListener('resize', function() {
     fixedNavbar.style.width = fakeNavbar.offsetWidth + 'px';
-});
-window.addEventListener('scroll', function () {
-    console.log(window.pageYOffset, 'st');
 });
 
 detailsProfession.forEach(function(element) {
@@ -43,67 +45,160 @@ detailsProfession.forEach(function(element) {
       }
     });
 });
-
 if (clientWidth > 768) {
     var url = window.location.href;
-
     // solution to keep changed style on refresh
     if (url !== 'http://127.0.0.1:8080/') {
-        console.log(clientHeight, 'chsa');
         sectionWelcome.style.display = 'none';
-        body.style.overflow = 'scroll';
-        body.style.height = '100%';
-        html.style.overflow = 'hidden';
-        html.style.height = '100%';
+        contentWrapper.style.display = 'block';
         fakeNavbar.style.height =  fixedNavbar.offsetHeight + 'px';
         fixedNavbar.style.width = fakeNavbar.offsetWidth + 'px';
         fakeNavbar.style.opacity = '0'; 
         fixedNavbar.style.position = 'fixed';
         fixedNavbar.style.top = '0';
         fixedNavbar.style.left = '0';
-    }
 
-    //setting hight to all elements with class .anchor
-    anchor.forEach(function (element) {
-        element.style.height = fixedNavbar.offsetHeight + 5 + 'px';
-        element.style.marginTop = -fixedNavbar.offsetHeight + 5 + 'px';
-        console.log(element.style.marginTop,'kk');
-    });
+        //setting hight to all elements with class .anchor
+        anchor.forEach(function (element) {
+            element.style.height = fixedNavbar.offsetHeight + 10 + 'px';
+            element.style.marginTop = -fixedNavbar.offsetHeight + 10 + 'px';
+        });
 
-    addPadding.forEach(function (element) {
-        element.addEventListener('click', function () {
+            var scrollTop = window.pageYOffset;
+            var sectionContactPosition = sectionContact.getBoundingClientRect().y;
+            var sectionPresentationPosition = sectionPresentation.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+            var sectionProfessionPosition = sectionProfession.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+            var sectionLinksPosition  = sectionLinks.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+            var sectionGalleryPosition = sectionGallery.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+
+            /* window.addEventListener('scroll', handlerScrollTop); */
+
+    function handlerScrollTop() {
+        scrollTop = window.pageYOffset;
+        if (scrollTop < sectionPresentationPosition) {
             addPadding.forEach(function (element) {
                 if (element.classList.contains('selected')) {
                     element.classList.remove('selected');
                 }
             });
-            this.classList.add('selected');
-        });
-    });
-    
+            contact.classList.add('selected');
+        } else if (scrollTop > sectionPresentationPosition && scrollTop < sectionProfessionPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            presentation.classList.add('selected');
+        } else if (scrollTop > sectionProfessionPosition && scrollTop < sectionLinksPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            profession.classList.add('selected');
+        } else if (scrollTop > sectionLinksPosition && scrollTop < sectionGalleryPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            links.classList.add('selected');
+        } else if (scrollTop > sectionGalleryPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            console.log()
+            gallery.classList.add('selected');        
+        }
+    }
+
+    }
+
    var affectedElement = [];
-    console.log(affectedElement);
     
     changeOverflow.forEach(function(element) {
         affectedElement.push(element);
     });
                              // affectedElement.push(downButton);
+
+    var scrollTop = window.pageYOffset;
+    var sectionContactPosition = sectionContact.getBoundingClientRect().y;
+    var sectionPresentationPosition = sectionPresentation.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+    var sectionProfessionPosition = sectionProfession.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+    var sectionLinksPosition  = sectionLinks.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+    var sectionGalleryPosition = sectionGallery.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+                         
     
     changeOverflow.forEach(function(element) {
         element.addEventListener('click', function() {
             sectionWelcome.style.display = 'none';
-            body.style.overflow = 'scroll';
-            body.style.height = '100%';
-            html.style.overflow = 'hidden';
-            html.style.height = '100%';
+            contentWrapper.style.display = 'block';
             fakeNavbar.style.height =  fixedNavbar.offsetHeight + 'px';
             fixedNavbar.style.width = fakeNavbar.offsetWidth + 'px';
             fakeNavbar.style.opacity = '0'; 
             fixedNavbar.style.position = 'fixed';
             fixedNavbar.style.top = '0';
             fixedNavbar.style.left = '0';
+            anchor.forEach(function (element) {
+                element.style.height = fixedNavbar.offsetHeight + 10 + 'px';
+                element.style.marginTop = -fixedNavbar.offsetHeight + 10 + 'px';
+            });
+            var scrollTop = window.pageYOffset;
+            var sectionContactPosition = sectionContact.getBoundingClientRect().y;
+            var sectionPresentationPosition = sectionPresentation.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+            var sectionProfessionPosition = sectionProfession.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+            var sectionLinksPosition  = sectionLinks.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+            var sectionGalleryPosition = sectionGallery.getBoundingClientRect().y - fixedNavbar.offsetHeight;
+ 
+            /* window.addEventListener('scroll', handlerScrollTop); */
+
+    function handlerScrollTop() {
+        scrollTop = window.pageYOffset;
+        if (scrollTop < sectionPresentationPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            contact.classList.add('selected');
+        } else if (scrollTop >= sectionPresentationPosition && scrollTop < sectionProfessionPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            presentation.classList.add('selected');
+        } else if (scrollTop >= sectionProfessionPosition && scrollTop < sectionLinksPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            profession.classList.add('selected');
+        } else if (scrollTop > sectionLinksPosition && scrollTop < sectionGalleryPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            links.classList.add('selected');
+        } else if (scrollTop >= sectionGalleryPosition) {
+            addPadding.forEach(function (element) {
+                if (element.classList.contains('selected')) {
+                    element.classList.remove('selected');
+                }
+            });
+            console.log()
+            gallery.classList.add('selected');        
+        }
+    }
         });
     }); 
+
+  
+    
 
     showInfoLara.addEventListener('click', function() {
         
@@ -111,7 +206,6 @@ if (clientWidth > 768) {
             infoLara.style.display = 'block';
             showInfoLara.innerHTML = '<i class="fas fa-minus-circle heading-14 ml-5 animated rotateIn"></i>';
             sectionContact.style.height = sectionContact.offsetHeight + 40 + 'px';
-            console.log(sectionContact.offsetHeight, 'heh');
             infoLara.classList.remove('fadeOut');
             infoLara.classList.add('animated', 'fadeIn');
             showInfoLara.classList.add('open');
@@ -129,7 +223,6 @@ if (clientWidth > 768) {
         
         if (!showInfoSabina.classList.contains('open')) {
             infoSabina.style.display = 'block';
-            console.log('click sab');
             showInfoSabina.innerHTML = '<i class="fas fa-minus-circle heading-14 ml-5 animated rotateIn"></i>';
             var sectionContact = document.querySelector('#section-contact');
             sectionContact.style.height = sectionContact.offsetHeight + 173 + 'px';
@@ -158,6 +251,31 @@ if (clientWidth <= 768) {
     var mobileLinksLink = document.querySelector('#fkn-links');
     var mobileGalleryLink = document.querySelector('#fkn-gallery');
     var counter = 0;
+    var mobileMenu = document.querySelector('#mobile-menu');
+    var mobileMenuLogo = document.querySelector('#mobile-menu-logo');
+    var mobileClientHeight = window.innerHeight;
+    var scrollFromTop = window.pageYOffset;
+
+
+    anchor.forEach(function (element) {
+        element.style.height = mobileMenu.offsetHeight + 10 + 'px';
+        element.style.marginTop = -mobileMenu.offsetHeight + 10 + 'px';
+    });
+
+
+    console.log(mobileClientHeight, 'mh');
+
+    window.addEventListener('scroll', function () {
+        scrollFromTop = window.pageYOffset;
+        if (scrollFromTop > mobileClientHeight) {
+            mobileMenu.style.backgroundImage = "url('/assets/img/homepage.jpg')";
+            mobileMenuLogo.style.backgroundImage = "url('/assets/img/logo.png')";
+        }
+        if (scrollFromTop < mobileClientHeight) {
+            mobileMenu.style.backgroundImage = "url()";
+            mobileMenuLogo.style.backgroundImage = "url()";
+        }
+    });
 
     showInfoLara.addEventListener('click', function() {
         
@@ -220,6 +338,7 @@ if (clientWidth <= 768) {
             downButton.style.display = 'block';
         } else {
             mobileSection.style.display = 'block';
+            mobileSection.style.position = 'fixed';
             body.style.overflowY = 'hidden';
             welcomeLogo.style.display = 'none';
             downButton.style.display = 'none';
